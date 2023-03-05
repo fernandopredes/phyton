65bbd383ed956774432c9b87b814c44ec308c558
fernando predes <fernando_predes@hotmail.com>
2023-03-04 22:38:40 -0300

    oop

--- cod.py
+++ cod.py
@@ -1,1 +1,30 @@
+#mostrar no console
 print("Hello World")
+
+# variaveis
+string = "Aluno"
+inteiro = 4
+um_float = 7.858
+um_booleano = True
+
+print(um_float)
+print(type(um_float))
+
+#potencia
+resultado_1 = 2 ** 4
+#resto
+resultado_2 = 13 % 4
+
+#parsing
+
+print("resultado da soma = " + str(4 + 4))
+print("potencia de 2 a 4 = " + str(resultado_1))
+print("resto da divisão de 13 por 4 = " + str(resultado_2))
+
+#decremento e incremento
+
+um_inteiro = 7
+um_inteiro -= 3
+print("resultado: " + str(um_inteiro))
+um_inteiro += 5
+print("resultado: " + str(um_inteiro))
