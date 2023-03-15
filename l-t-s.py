1282244d1c47004ed0bd61fb5374a124b543dbac
fernando predes <fernando_predes@hotmail.com>
2023-03-15 18:22:07 -0300

    lists tuples sets

---
+++ l-t-s.py
@@ -0,0 +1,9 @@
+#list => can modify
+l = ['bob', 'rolf', 'anne']
+#tuples => cannot modify
+t = ('bob','rolf','anne')
+#set => cant have duplicate elements / dont keep the order of the elements
+s = {'bob','rolf','anne'}
+
+l.append('smith')
+print(l)
