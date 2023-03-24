978aed03cfa02cb5a463e2bad617da55b67a23da
fernando predes <fernando_predes@hotmail.com>
2023-03-24 19:56:19 -0300

    decorated responses

--- docker/resources/store.py
+++ docker/resources/store.py
@@ -9,6 +9,7 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route('/store/<string:store_id>')
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -23,16 +24,20 @@ class Store(MethodView):
             abort(404, message="Store not found.")
 
 @blp.route('/store')
+
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores":list(stores.values())}
+        return stores.values()
 
     @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
         for store in stores.values():
             if store_data['name'] == store['name']:
                 abort(400, message = f"Store already exists")
+
         store_id = uuid.uuid4().hex
         store = {**store_data, "id":store_id}
         stores[store_id] = store
-        return store, 201
+        return store
