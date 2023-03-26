308378cdad995faf0303d495b84db58dfb020269
fernando predes <fernando_predes@hotmail.com>
2023-03-26 00:51:17 -0300

    finished one-to-many

--- docker/resources/item.py
+++ docker/resources/item.py
@@ -26,7 +26,7 @@ class Item(MethodView):
             item.name = item_data['name']
         else:
             item = ItemModel(id=item_id, **item_data)
-            
+
         db.session.add(item)
         db.session.commit()
 
@@ -34,13 +34,15 @@ class Item(MethodView):
 
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Deleting an item is not impletemented")
+        db.session.delete(item)
+        db.session.commit()
+        return {"message":"Item deleted"}, 200
 
 @blp.route('/item')
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
