aad379ef21cf455c1c06a2dce4aadbffab0a79b8
fernando predes <fernando_predes@hotmail.com>
2023-03-27 18:29:07 -0300

    created jwt

--- docker/resources/user.py
+++ docker/resources/user.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
@@ -8,11 +9,11 @@ from schemas import UserSchema
 
 blp = Blueprint("Users", "users", description="Operations on Users")
 
-blp.route("/register")
-class UserRegister(dump_only=True):
+@blp.route("/register")
+class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"].first()):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
             abort(409, message="A user with this username already exists;")
 
         user = UserModel(
@@ -23,3 +24,36 @@ class UserRegister(dump_only=True):
         db.session.commit()
 
         return {"message" : "User created"}, 201
+
+@blp.route("/users")
+class Users(MethodView):
+    @blp.response(200, UserSchema(many=True))
+    def get(self):
+        return UserModel.query.all()
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message":"User deleted."},200
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data['username']
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data['password'], user.password):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+
+        abort(401, message="Invalid cedentials.")
